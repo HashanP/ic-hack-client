b86907b72de2975c988fc9b690a92851cfd5936a
julieEmile <jce317@ic.ac.uk>
2018-01-27 13:24:47 +0000

    Initial Commit

---
+++ Musique/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Musique
+//
+//  Created by Julie Emile on 27/01/2018.
+//  Copyright © 2018 Julie Emile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
