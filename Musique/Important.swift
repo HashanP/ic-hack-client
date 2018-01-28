1f7afcb1c88dd80447f1062422071dc731340e3d
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2018-01-28 05:08:04 +0000

    File structure changes

---
+++ Musique/Important.swift
@@ -0,0 +1,15 @@
+//
+//  Important.swift
+//  Musique
+//
+//  Created by Hashan Punchihewa on 28/01/2018.
+//  Copyright © 2018 Julie Emile. All rights reserved.
+//
+
+import Foundation
+
+class Important {
+    static func getPlaylistNames() -> [String] {
+        
+    }
+}
